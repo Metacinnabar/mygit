d4f28b953fe9daf5cd109603b2de1fc28b4c83e0
GoodPro712 <45357714+GoodPro712@users.noreply.github.com>
2021-09-06 17:10:22 +1000

    Fix small issues

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 if ! [ -x "$(command -v lice)" ]; then
-    read -p "Do you wish to install lice (via pip)? [Y/n]" reply
+    read -r -p "Do you wish to install lice (via pip)? [Y/n]" reply
     case $reply in
-        [Yy]* ) pip install lice; break;;
+        [Yy]* ) pip install lice;;
         * ) echo "Exitting..."; exit;;
     esac
 fi
 
-echo "Copying files to /usr/local/bin/"
-if [ -f /usr/local/bin/mygit ]; then
-    sudo rm -f /usr/local/bin/mygit
+echo "Copying mygit to /usr/bin/"
+if [ -f /usr/bin/mygit ]; then
+    sudo rm -f /usr/bin/mygit
 fi
 
-sudo cp ./mygit /usr/local/bin/
+sudo cp ./mygit /usr/bin/
 exit 0
